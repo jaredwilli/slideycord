aca2f882290d5041cba0ddd8d891bc7c1ef97a4a
jaredwilli <jaredwilli@gmail.com>
2012-12-31 18:22:00 -0500

    added in the problem text for a little content explaining the rules

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-	/**
+/**
  * Slider Development Test for Beach Body Interview
  *
  * By: Jared Williams
@@ -20,35 +20,39 @@ var Slider = {
 			bullets = $('.bullets');
 
 		// Setup the initial state of the activeSlide, section, bullet and arrows
+		//$(Slider.activeSlide).animate({ opacity: 0 });
 		$(Slider.activeSection).addClass('active').find('.expand').text('-');
-		$('.thumbs, .bullets, .images').find('li:first').addClass('active').show();
+		$('.thumbs, .bullets', Slider.activeSection).find('li:first').addClass('active').show();
 
 		document.getElementById('prev').href = Slider.prevSlide;
 		document.getElementById('next').href = Slider.nextSlide;
+
+		Slider.moveToSlide();
 
 		// Handle clicks on the sections of accordion
 		$('h1').on('click', function(e) {
 			e.preventDefault();
 			if ($(this).parent().hasClass('active')) return;
+			$(Slider.activeSlide).animate({ opacity: 0 }).removeClass('active');
 
 			Slider.activeSection = '#' + $(this).parent().attr('id');
-			Slider.activeSlide = $(Slider.activeSection).find('.thumbs > li:first a').attr('href').split('t')[0];
+			Slider.activeSlide = $(Slider.activeSection).find('.thumbs > li:first a').attr('href');
 
 			Slider.collapse();
 			Slider.updateArrows();
 			Slider.moveToSlide();
 		});
 
-
 		// Handle clicks on thumbnails
 		$('li', thumbs).find('a').on('click', function(e) {
 			e.preventDefault();
 			if ($(this).parent().hasClass('active')) return;
-
-			Slider.activeSlide = $(this).attr('href');
+			$(Slider.activeSlide).animate({ opacity: 0 }).removeClass('active');
 
 			$('.thumbs, .images').find('li').removeClass('active');
 			$(this).parent().addClass('active');
+
+			Slider.activeSlide = $(this).attr('href');
 
 			Slider.updateArrows();
 			Slider.moveToSlide();
@@ -58,8 +62,10 @@ var Slider = {
 		$(bullets).find('a').on('click', function(e) {
 			e.preventDefault();
 			if ($(this).parent().hasClass('active')) return;
+			$(Slider.activeSlide).animate({ opacity: 0 }).removeClass('active');
 
 			Slider.activePage = $(this).attr('href');
+			Slider.activeSlide = $(Slider.activePage).find('a').attr('href');
 
 			Slider.goToPage();
 			Slider.updateArrows();
@@ -69,21 +75,18 @@ var Slider = {
 		// Handle clicks on the next / previous links
 		$('.arrows').find('a').on('click', function(e) {
 			e.preventDefault();
+			$(Slider.activeSlide).animate({ opacity: 0 }).removeClass('active');
 
-
-			if ($(Slider.activeSlide).hasClass('last')) {
-				Slider.activeSlide = '#' + $('li:last', '.images').attr('id');
-				Slider.activeSection = $('a[href='+ Slider.activeSlide +']').parents('section').attr('id');
-				Slider.collapse();
-			}
-
+			Slider.activeSection = '#' + $('a[href='+ Slider.activeSlide +']').parents('section').attr('id');
 			Slider.activeSlide = $(this).attr('href');
+			//Slider.activePage
+
+			var index = $(Slider.activeSlide, '.images').index();
 
 			Slider.updateArrows();
-			//Slider.whatNow();
+			Slider.whatNow( index );
 			Slider.moveToSlide();
 		});
-
 	},
 
 	/**
@@ -93,8 +96,6 @@ var Slider = {
 	 * And change the - to a + in the H1 tag
 	 */
 	collapse: function() {
-		console.log(Slider);
-
 		$('.thumbs, .bullets').find('li').removeClass('active');
 		$('section').removeClass('active').find('.expand').text('+');
 		$('.thumbs', 'section').css({ left: 0 });
@@ -109,12 +110,54 @@ var Slider = {
 	 * and if necessary, make first bullet of thumbnail page nav active
 	 */
 	expand: function() {
-		console.log(Slider);
-
 		// Need to get the index of the first thumbnail of the expanded section
 		$('.thumbs, .bullets', Slider.activeSection).find('li:first').addClass('active').show();
 		$(Slider.activeSection).addClass('active').find('.expand').text('-');
 	},
+	/**
+	 * WhatNow is called by the next/previous arrow links to check
+	 * whether or not other actions should be done
+	 *
+	 * Calls goToPage if activeSlide thumbnail has an ID attribute
+	 * Calls collapse method if the ID of the parent section of the activeSlide changes
+	 */
+	whatNow: function(index) {
+		Slider.activePage = '#group-0';
+
+		if (index === 11) {
+			Slider.activeSection = '#lips';
+			Slider.activeSlide = '#' + $('.images li').eq(index).attr('id');
+		}
+		else if (index === 9 || index === 10) {
+			Slider.activeSection = '#forehead';
+			Slider.activeSlide = '#' + $('.images li').eq(index).attr('id');
+		}
+		else if (index >= 0 || index <= 8) {
+			Slider.activeSection = '#eyes';
+
+			// Handle which page to make active for bullet nav
+			if (index >= 0 && index <= 2) {
+				Slider.activePage = '#group-0';
+				Slider.goToPage();
+			} else if (index >= 3 && index <= 5) {
+				Slider.activePage = '#group-1';
+				Slider.goToPage();
+			} else if (index >= 6 && index <= 8) {
+				Slider.activePage = '#group-2';
+				Slider.goToPage();
+			}
+		}
+		Slider.activeSlide = '#' + $('.images').find('li').eq(index).attr('id');
+
+		$('section').removeClass('active').find('.expand').text('+');
+		$('.thumbs').find('li').removeClass('active');
+
+		$(Slider.activeSection).addClass('active').find('.expand').text('-');
+		$('.thumbs a[href='+ Slider.activeSlide +']').parent('li').addClass('active');
+	},
+	/**
+	 * Update the next/previous links with the correct slide href's
+	 */
 	updateArrows: function() {
 		// Set the next/previous href values according to the activeSlide
 		Slider.prevSlide = '#' + $(Slider.activeSlide).prev().attr('id');
@@ -131,27 +174,6 @@ var Slider = {
 
 		document.getElementById('prev').href = Slider.prevSlide;
 		document.getElementById('next').href = Slider.nextSlide;
-		console.log(Slider.prevSlide, Slider.nextSlide);
-	},
-	/**
-	 * WhatNow is called by the next/previous arrow links to check
-	 * whether or not other actions should be done
-	 *
-	 * Calls goToPage if activeSlide thumbnail has an ID attribute
-	 * Calls collapse method if the ID of the parent section of the activeSlide changes
-	 */
-	whatNow: function() {
-		console.log(Slider);
-
-		$('.thumbs').find('li').removeClass('active');
-
-		$('a[href='+ Slider.activeSlide +']').parent().addClass('active');
-
-		if ($('.thumbs .active').attr('id') !== Slider.activePage) {
-			Slider.activePage = '#'+ $('.thumbs .active').attr('id');
-			Slider.goToPage();
-		}
-
 	},
 	/**
 	 * GoToPage called when clicking on bullet nav icons
@@ -161,15 +183,9 @@ var Slider = {
 	 * Sets the activeSlide to show the corresponding image
 	 */
 	goToPage: function() {
-		console.log(Slider);
-
-		Slider.activeSlide = $(Slider.activePage).find('a').attr('href');
-
-		$('.thumbs, .bullets, .images').find('li').removeClass('active');
-		$('a[href='+ Slider.activePage +']').parent().addClass('active')
+		$('.thumbs, .bullets').find('li').removeClass('active');
+		$('a[href='+ Slider.activePage +']').parent().addClass('active');
 		$(Slider.activePage).addClass('active');
-
-		//$(Slider.activeSlide).addClass('active');
 
 		if ($('#group-0').hasClass('active')) {
 			$('#group-0').parent().css({ left: 0 +'px' });
@@ -188,10 +204,8 @@ var Slider = {
 	 * Always called last
 	 */
 	moveToSlide: function() {
-		console.log(Slider);
-
 		$('li', '.images').removeClass('active');
-		$(Slider.activeSlide).addClass('active');
+		$(Slider.activeSlide).animate({ opacity: 1 }).addClass('active');
 	}
 };
 Slider.init();
